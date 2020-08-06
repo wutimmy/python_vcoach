c20e0805b9c90168488455a59624cafcc7511d11
timmyw <wutimmy01@gmail.com>
2020-08-06 10:48:59 +0800

    nearly finish muti hands function

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import cv2
 from tools.OpenPose import OpenPose
+import traceback
+import sys
 
-xys={}
+exc_info=""
+p1=[]
+p2=[]
+p3=[]
+def left():
+    global p1, p2, p3
+    p1=lh[0]
+    p2=lh[1]
+    p3=lh[3]
+    print("left")
+    cv2.putText(frame,"Detected Left Hand.",(40, 120), cv2.FONT_HERSHEY_SIMPLEX,  1, (255, 0, 0), 2, cv2.LINE_AA)
+
+def right():
+    global p1, p2, p3
+    p1=rh[0]
+    p2=rh[1]
+    p3=rh[3]
+    print("right")
+    cv2.putText(frame,"Detected Right Hand.",(40, 120), cv2.FONT_HERSHEY_SIMPLEX,  1, (255, 0, 0), 2, cv2.LINE_AA)
 
 if __name__ == "__main__":
     pose = OpenPose()
@@ -14,53 +34,77 @@ if __name__ == "__main__":
         if not success:
             continue
 
-        people = pose.detect(frame, in_height=100)
+        people = pose.detect(frame, in_height=150)
         for person in people:
             lh,rh=pose.draw_hand(frame, person)
         print(lh,rh)
         
         try:
-            p1=rh[0]
-            p2=rh[1]
-            p3=rh[3]
+            exc_info = sys.exc_info()
             """
-            if lh is None and rh is not None:
-                p1=rh[0]
-                p2=rh[1]
-                p3=rh[3]
-            elif lh is not None and rh is None:
-                p1=lh[0]
-                p2=lh[1]
-                p3=lh[3]
-            elif lh is None and rh is None:
+            p1=lh[0]
+            p2=lh[1]
+            p3=lh[3]
+            """
+            
+            if bool(len(lh)) is False and bool(len(rh)) is True:
+                right()
+            elif bool(len(lh)) is True and bool(len(rh)) is False:
+                left()
+            elif bool(len(lh)) is False and bool(len(rh)) is False:
+                print("No hands detected.")
+                cv2.putText(frame,"No hands detected.",(40, 160), cv2.FONT_HERSHEY_SIMPLEX,  1, (0, 0, 255), 2, cv2.LINE_AA)
                 cv2.imshow("frame", frame)
-                cv2.waitKey(1)
-                continue
+                key_code = cv2.waitKey(1)
+                if key_code in [27, ord('q')]:
+                    break
+                else:
+                    continue
+            elif bool(len(lh)) is True and bool(len(rh)) is True:
+                print("Both hands detected.")
+                #cv2.putText(frame,"Both hands detected. Testing...",(40, 180), cv2.FONT_HERSHEY_SIMPLEX,  1, (0, 0, 255), 2, cv2.LINE_AA)
+                
+                if lh[0][0] < rh[0][0] and lh[0][1] > rh[0][1]:
+                    left()
+                else:
+                    right()
+
+                """
+                cv2.imshow("frame", frame)
+                key_code = cv2.waitKey(1)
+                if key_code in [27, ord('q')]:
+                    break
+                """
+
             else:
+                print("else")
                 cv2.imshow("frame", frame)
-                cv2.waitKey(1)
-                continue
-            """
+                key_code = cv2.waitKey(1)
+                if key_code in [27, ord('q')]:
+                    break
+                else:
+                    continue
+            
             k1=(p2[1]-p1[1])/(p2[0]-p1[0])
             k2=(p3[1]-p2[1])/(p3[0]-p2[0])
             print(k1-k2)
             print(k1,k2)
             if abs(k1-k2) < 0.5:
                 print("hand straight.")
-                cv2.putText(frame,"hand straight.",(40, 40), cv2.FONT_HERSHEY_SIMPLEX,  1, (0, 255, 255), 1, cv2.LINE_AA)
+                cv2.putText(frame,"hand straight.",(40, 40), cv2.FONT_HERSHEY_SIMPLEX,  1, (0, 255, 255), 2, cv2.LINE_AA)
             else:
                 print("hand is not straight.")
-                cv2.putText(frame,"hand is not straight.",(40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 1, cv2.LINE_AA)
-            if abs(k1) < 0.4 and abs(k2) < 0.4:
+                cv2.putText(frame,"hand is not straight.",(40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
+            if abs(k1) < 0.35 and abs(k2) < 0.35:
                 print("hand is level.")
-                cv2.putText(frame,"hand is level.",(40, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
+                cv2.putText(frame,"hand is level.",(40, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
             else:
                 print("hand is not level.")
-                cv2.putText(frame,"hand is not level.",(40, 80), cv2.FONT_HERSHEY_SIMPLEX,1, (0, 0, 255), 1, cv2.LINE_AA)
+                cv2.putText(frame,"hand is not level.",(40, 80), cv2.FONT_HERSHEY_SIMPLEX,1, (0, 0, 255), 2, cv2.LINE_AA)
 
                 
-        except Exception as e:
-            print(e)
+        except Exception:
+            print(traceback.format_exc())
         
         cv2.imshow("frame", frame)
 
