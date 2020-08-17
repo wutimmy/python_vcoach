bb5210cbf1785c6301d13f8511a701c7e423cea4
timmyw <wutimmy01@gmail.com>
2020-08-17 22:12:10 +0800

    finished hand angle calculation

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ exc_info=""
 p1=[]
 p2=[]
 p3=[]
-lineA=[]
-lineB=[]
-error=False
+
 
 
 def left():
@@ -19,11 +17,7 @@ def left():
     p2=lh[1]
     p3=lh[3]
     print("left")
-    try:
-        lineA=[lh[0],lh[1]]
-        lineB=[lh[2],lh[3]]
-    except:
-        error = True
+
     
     cv2.putText(frame,"Detected Left Hand.",(40, 120), cv2.FONT_HERSHEY_SIMPLEX,  1, (255, 0, 0), 2, cv2.LINE_AA)
 
@@ -34,17 +28,8 @@ def right():
     p3=rh[3]
     print("right")
     cv2.putText(frame,"Detected Right Hand.",(40, 120), cv2.FONT_HERSHEY_SIMPLEX,  1, (255, 0, 0), 2, cv2.LINE_AA)
-    try:
-        lineA=[rh[0],rh[1]]
-        lineB=[rh[2],rh[3]]
-    except:
-        error = True
 
-def slope(x1, y1, x2, y2): # Line slope given two points:
-    return (y2-y1)/(x2-x1)
 
-def angle(s1, s2): 
-    return math.degrees(math.atan((s2-s1)/(1+(s2*s1))))
 
 if __name__ == "__main__":
     pose = OpenPose()
@@ -63,18 +48,10 @@ if __name__ == "__main__":
         for person in people:
             lh,rh=pose.draw_hand(frame, person)
         print(lh,rh)
-        """
-        for person in people1:
-            lh,rh=pose.draw_hand(body_frame, person)
-        """
+
         
         try:
             exc_info = sys.exc_info()
-            """
-            p1=lh[0]
-            p2=lh[1]
-            p3=lh[3]
-            """
             
             if bool(len(lh)) is False and bool(len(rh)) is True:
                 right()
@@ -103,37 +80,19 @@ if __name__ == "__main__":
                 else:
                     right()
 
-            
 
-                """
-                cv2.imshow("frame", frame)
-                key_code = cv2.waitKey(1)
-                if key_code in [27, ord('q')]:
-                    break
-                """
 
-            else:
-                print("else")
-                cv2.imshow("frame", frame)
-                #cv2.imshow("body", body_frame)
-                key_code = cv2.waitKey(1)
-                if key_code in [27, ord('q')]:
-                    break
-                else:
-                    continue
 
             print("*************")
             print(lineA)
             print(lineB)
+            ang_a = math.atan2(p3[1]-p2[1],p3[0]-p2[0])*180/math.pi
+            ang_b = math.atan2(p2[1]-p1[1],p2[0]-p1[0])*180/math.pi
+            ang = round(180 + ang_a - ang_b)
+            print(ang)
+            cv2.putText(frame,str(ang),(40, 180), cv2.FONT_HERSHEY_SIMPLEX,1, (0, 0, 255), 2, cv2.LINE_AA)
             print("*************")
             
-            if not error:
-                slope1 = slope(lineA[0][0], lineA[0][1], lineA[1][0], lineA[1][1])
-                slope2 = slope(lineB[0][0], lineB[0][1], lineB[1][0], lineB[1][1])
-                ang = angle(slope2, slope1)
-                print("-"*10+"hand angle"+"-"*10)
-                print(ang)
-                print("-"*10+"hand angle"+"-"*10)
             
             k1=(p2[1]-p1[1])/(p2[0]-p1[0])
             k2=(p3[1]-p2[1])/(p3[0]-p2[0])
